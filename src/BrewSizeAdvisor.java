0a2816fb0c4f24ea21827facb3af9e0fa5b13aec
MADHAVAN S <mathavanmathav123@gmail.com>
2026-01-19 15:53:24 +0530

    Update BrewSizeAdvisor.java

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -3,14 +3,15 @@ public class BrewSizeAdvisor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int volume = sc.nextInt();
-        if (volume>=0 && volume<200){
+        if (volume>= 0 && volume<200){
             System.out.println("Small");
         }else if(volume>=200 && volume<=350){
             System.out.println("Medium");
-        }else{
+        }else if(volume>350){
             System.out.println("Large");
-                }
-
+                }else{
+            System.out.println("Invalid");
+        }
         // TODO:
         // Print "Small", "Medium", or "Large"
         // based on the given volume
