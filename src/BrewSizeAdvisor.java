874f5a561defc6a64ab0ea523f6cdbcbb31100a8
MADHAVAN S <mathavanmathav123@gmail.com>
2026-01-19 15:37:56 +0530

    Implement brew size classification based on volume
    Add size classification for brew volume input.

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
-
 public class BrewSizeAdvisor {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int volume = sc.nextInt();
+        if (volume>=0 && volume<200){
+            System.out.println("SMALL")
+        }else if(volume>=200 && volume<=350){
+            System.out.println("MEDIUM")
+        }else{
+            System.out.println("LARGE")
+                }
 
         // TODO:
         // Print "Small", "Medium", or "Large"
